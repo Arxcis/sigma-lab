9700066cd2dbfdc709677534c29af5d708b3ac36
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2017-07-25 20:44:45 +0200

    Kommer godt i gang. Prøver å utvikle abstraksjonen trail.js denne gang. Virker mye ryddigere så langt. Vi får se hvor langt det holder

--- 240717-d3-svg-line/app.js
+++ 240717-d3-svg-line/app.js
@@ -9,32 +9,51 @@ window.addEventListener('load', () => {
                      { "x": 40,  "y": 10}, { "x": 60,  "y": 40},
                      { "x": 80,  "y": 5},  { "x": 100, "y": 60}];
 
-    //This is the accessor function we talked about above
+    // Get bounding rect
+    let squareRect = document.querySelector('.square-in-the-middle').getBoundingClientRect();
+
     let path = d3.path();
+    let pathQB = d3.path();
 
-    let startx = 50;
-    let starty = 50;
+    let startx = squareRect.x + squareRect.width/2;
+    let starty = squareRect.y + squareRect.height;
+
+    // Creating a normal line from point to point
     path.moveTo(startx, starty);
-
     lineData.forEach(data => {
         path.lineTo(startx + data.x, starty + data.y);
     });
 
-    //The SVG Container
-    let body         = document.querySelector('body');
+    // Creating a quadratic bezier curved line
+    pathQB.moveTo(startx, starty);
+    pathQB.bezierCurveTo(startx, starty+100, startx+100, starty, startx+100, starty+100);
+
+    // The SVG Container
     let svgContainer = document.createElementNS(svgNamespace, 'svg');
-    svgContainer.setAttribute('width', 200);
-    svgContainer.setAttribute('height', 200);
-    body.appendChild(svgContainer);
+    svgContainer.style.position = 'absolute';
+    svgContainer.style.top  = '0px';
+    svgContainer.style.left = '0px';
+
+    svgContainer.setAttribute('width', 1000);
+    svgContainer.setAttribute('height', 1000);
+    document.body.appendChild(svgContainer);
 
 
     //The line SVG Path we draw
-
     let svgPath = document.createElementNS(svgNamespace, 'path'); 
     svgPath.setAttribute('d', path.toString());
     svgPath.setAttribute('stroke', 'blue');
-    svgPath.setAttribute('strok-width', 2);   
+    svgPath.setAttribute('stroke-width', 2);   
     svgPath.setAttribute('fill', 'none');   
+
+    let svgPathQB = document.createElementNS(svgNamespace, 'path'); 
+    svgPathQB.setAttribute('d', pathQB.toString());
+    svgPathQB.setAttribute('stroke', 'red');
+    svgPathQB.setAttribute('stroke-width', 2);   
+    svgPathQB.setAttribute('fill', 'none'); 
+
     svgContainer.appendChild(svgPath);
+    svgContainer.appendChild(svgPathQB);
+
 });
 
---
+++ 250717-full-scale-bezier/trail.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+function makeTrail() {
+    return {
+        container: document.querySelector('main'),
+        activeLevel: document.body,
+        activeNode: document.body,
+    }    
+}
+
+function makeLevel(trail) {
+    let level = document.createElement('div');
+    level.classList.add('level', 'selected');
+
+    trail.container.appendChild(level);
+    trail.activeLevel.classList.remove('selected');
+    trail.activeLevel = level;
+
+    return level;
+}
+
+function makeNode(trail){
+    let node  = document.createElement('div');
+    node.classList.add('node', 'selected');
+
+    node.addEventListener('click', (e) => {
+        console.log('hello');
+        trail.activeNode.classList.remove('selected');
+        trail.activeNode = node;
+        trail.activeNode.classList.add('selected');
+
+        trail.activeLevel.classList.remove('selected');
+        trail.activeLevel = trail.activeNode.parentElement;
+        trail.activeLevel.classList.add('selected');
+    });
+
+    trail.activeLevel.appendChild(node);
+    trail.activeNode.classList.remove('selected');
+    trail.activeNode = node;
+
+    return node;
+}
